c2a40d4d20eaa1f7ce5db4babd67348554aaf58c
kunal00000 <kunalverma2468@gmail.com>
2024-03-22 02:25:25 +0530

    Add icon to window configuration

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: '',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
       enableRemoteModule: true,
     },
   });
-  win.webContents.openDevTools();
+
+  // win.webContents.openDevTools();
 
   win.loadFile('index.html');
 }
