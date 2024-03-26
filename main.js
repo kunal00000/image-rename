809d18b601d1422ec6631dabee03b90afeabf5c3
kunal00000 <kunalverma2468@gmail.com>
2024-03-27 05:08:01 +0530

    remove devtools

--- main.js
+++ main.js
@@ -4,15 +4,12 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: '',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
       enableRemoteModule: true,
     },
   });
-
-  // win.webContents.openDevTools();
 
   win.loadFile('index.html');
 }
